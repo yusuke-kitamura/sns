b2b39bb1c88c18697f7723780630fe0c34f2a26e
vagrant <vagrant@localhost.localdomain>
2020-04-23 15:17:24 +0000

    [update]投稿一覧と詳細を追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,14 +1,19 @@
 class PostImagesController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def new
 		@post_image = PostImage.new
 	end
 
 	def create
 		@post_image = PostImage.new(post_image_params)
-		@post_image.save
-		redirect_to post_images_path
+		@post_image.user_id = current_user.id
+		if @post_image.save
+			flash[:notice] = "投稿成功"
+		    redirect_to post_image_path(@post_image)
+	   else
+		render action: :new
+	end
 	end
 
 	def index
@@ -16,7 +21,7 @@ class PostImagesController < ApplicationController
 	end
 
 	def show
-		
+		@post_image = PostImage.find(params[:id])
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'post_images#index'
 
   resources :post_images
+  resources :users, only: [:index,:show,:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
